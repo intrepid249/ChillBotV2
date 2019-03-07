5c5fc9c29bf4fd31db58f79a440b4c959a003f9b
Jack McCall <jmccall3@sae.au>
2019-03-07 16:02:16 +1100

    Added warnings module

---
+++ ChillBotV2/Modules/Moderation/Warnings.cs
@@ -0,0 +1,23 @@
+﻿using ChillBotV2.Attributes;
+using ChillBotV2.Context;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChillBotV2.Modules.Moderation
+{
+    [AdminPrefix, Remarks("admin")]
+    [Summary("Provides administration commands used to formally issue people with warnings")]
+    [RequireUserPermission(Discord.GuildPermission.Administrator)]
+    public class Warnings : ModuleBase<PrefixCommandContext>
+    {
+        [Command("warn"), Alias("w")]
+        [Summary("Formally issue a specified user with a warning")]
+        public async Task WarnUser()
+        {
+
+        }
+    }
+}
